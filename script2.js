23f1a07a0a7683937eef059974cc6ed64e6d8214
andreabotta <50942916+andreabotta@users.noreply.github.com>
2019-05-23 15:56:17 +0200

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    var pageid=window.location.search.trim();
+    var itemSettings = {
+        "async":true,
+        "crossDomain":true,
+        "url":"https://my-json-server.typicode.com/andreabotta/jsonserver/singleitems"+pageid,
+        "method":"GET",
+        "dataType":"json"
+    }
+    $.ajax(itemSettings).done(function(response){
+        var myitem="";
+
+        $.each(response , function(i, obj){
+            myitem+='<div class="card '+obj.id+'" id="' +obj.id+'">'; 
+            myitem+='<a href="#" class="linkcard">';  
+            myitem+='<img class="card-img-top" src="'+obj.img+'" alt="Card image cap">"';
+            myitem+='<div class="card-body">';
+            myitem+='<h5 class="card-title"><h2>'+obj.title+'</h2</h5>'; 
+            myitem+='<p class="card-text"><h3>'+obj.price+'</h3></p>';
+            myitem+=''+obj.desc+'</div>';
+            myitem+='<div class="card-footer text-muted">';
+            myitem+='<h4>'+obj.brand+'</h4>';
+            myitem+='</div>';
+            myitem+='</a>';
+            myitem+='</div>';
+           
+          console.log('valore',obj.id);
+        });
+        $("#singleitem").append(myitem);
+ 
+    });
+
+    $.ajax(itemSettings).done(function(response){
+        var myitem="";
+
+        $.each(response , function(i, obj){
+           
+            myitem+='<li class="list-group-item active">'+obj.brand+'</li>',
+           
+          console.log('valore',obj.id);
+        });
+        $("#brandlist2").append(myitem);
+ 
+    });
+
+    
+ });
+ 
+ 
