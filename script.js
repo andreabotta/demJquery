73c85cb6afc94b580709dde1d8628f61342d2b64
andreabotta <50942916+andreabotta@users.noreply.github.com>
2019-05-28 16:36:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-   var itemSettings = {
-       "async":true,
-       "crossDomain":true,
-       "url":"https://my-json-server.typicode.com/andreabotta/jsonserver/items",
-       "method":"GET",
-       "dataType":"json"
-   }
-   $.ajax(itemSettings).done(function(response){
-       var myitem="";
-       $.each(response , function(i, obj){
-           myitem+='<div class="col-lg-4">';
-           myitem+='<div class="card '+obj.id+'" id="' +obj.id+'">';
-           myitem+='<a href="bootstrap2.html?id='+obj.id+'" class="linkcard">';
-           myitem+='<img class="card-img-top" src="'+obj.img+ '" alt="Card image cap" width="700px" height="230px">';
-           myitem+='<div class="card-body" style="height:150px;overflow:hidden">';
-           myitem+='<h5 class="card-title"> '+obj.title+'</h5>';
-           myitem+='<h5> '+obj.price+'</h5>';
-           myitem+='<p class="card-text">'+obj.desc+'</p>';
-           myitem+='</div>';
-           myitem+='<div class="card-footer text-muted"> '+obj.brand+' </div>';
-           myitem+='</a>';
-           myitem+='</div>';
-           myitem+='</div>';
-         console.log('valore',obj.id);
-       });
-       $("#listitems").append(myitem);
+    var itemSettings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://my-json-server.typicode.com/andreabotta/jsonserver/items",
+        "method": "GET",
+        "dataType": "json"
+    }
+    $.ajax(itemSettings).done(function (response) {
+        var myitem = "";
+        $.each(response, function (i, obj) {
+            console.log(obj.brand);
+            myitem += '<div class="col-lg-4">';
+            myitem += '<div class="card ' + obj.id + '" id="' + obj.id + '">';
+            myitem += '<a href="bootstrap2.html?id=' + obj.id + '" class="linkcard">';
+            myitem += '<img class="card-img-top" src="' + obj.img + '" alt="Card image cap" width="700px" height="230px">';
+            myitem += '<div class="card-body" style="height:150px;overflow:hidden">';
+            myitem += '<h5 class="card-title"> ' + obj.title + '</h5>';
+            myitem += '<h5> ' + obj.price + '</h5>';
+            myitem += '<p class="card-text">' + obj.desc + '</p>';
+            myitem += '</div>';
+            myitem += '<div class="card-footer text-muted"> ' + obj.brand + ' </div>';
+            myitem += '</a>';
+            myitem += '</div>';
+            myitem += '</div>';
+        });
+        $("#listitems").append(myitem);
 
-   });
-
-   var itemSettings = {
-    "async":true,
-    "crossDomain":true,
-    "url":"https://my-json-server.typicode.com/andreabotta/jsonserver/brands",
-    "method":"GET",
-    "dataType":"json"
-}
-$.ajax(itemSettings).done(function(response){
-    var myitem="";
-    $.each(response , function(i, obj){
-        myitem+=' <ul class="list-group">';
-        myitem+='<li class="list-group-item">'+obj.brand+'</li>';
-        myitem+='</ul>';
-        
-        
-      console.log('valore',obj.id);
     });
-    $("#brandlist").append(myitem);
 
-});
+    var itemSettings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://my-json-server.typicode.com/andreabotta/jsonserver/brands",
+        "method": "GET",
+        "dataType": "json"
+    }
+    $.ajax(itemSettings).done(function (response) {
+        var myitem = "";
+        $.each(response, function (i, obj) {
+            myitem += ' <ul class="list-group">';
+            myitem += '<a href="category.html?id=' + obj.brand + '" class="linkcard">';
+            myitem += '<li class="list-group-item">' + obj.brand + '</li>';
+            myitem += '</a>';
+            myitem += '</ul>';
+
+
+            console.log('valore', obj.id);
+        });
+        $("#brandlist").append(myitem);
+
+    });
+   $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
 });
 
